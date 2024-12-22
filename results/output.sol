21930c8cda93bc58d3f99e37a0fa21860e9a3ec0
jfrometa88 <jfrometa99@gmail.com>
2024-12-22 16:44:42 +0100

    Add files via upload

---
+++ results/output.sol
@@ -0,0 +1,23 @@
+# Solution for model Plan de produccion
+# Objective value = 9.0095291502100823e+05
+Cant._Product[MT] 90
+Cant._Product[CT] 50
+Cant._Product[MHT] 40
+Cant._Product[HT] 0
+Cant._Product[TE] 0
+Cant._Product[TT] 0
+Cant._Product[FT] 0
+Cant._Product[PT] 2.3906512605042010e+02
+Cant._Product[BT] 0
+Cant._Product[RT] 0
+Cant._Product[TA] 0
+Cant._Product[TiE] 0
+Cant._Product[TiEE] 100
+Cant._Product[FTi] 60
+Cant._Product[MTi] 0
+Cant._Product[FC] 0
+Cant._Product[RC] 0
+Cant._Product[TC] 0
+Cant._Product[MM] 35
+Cant._Product[CM] 70
+Cant._Product[MHM] 0
